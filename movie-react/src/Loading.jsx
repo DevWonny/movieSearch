a7becd9dacb9ff4b04f363038ec8dca3416fa7b8
DevWonny <cjfdnjs1994@naver.com>
2022-08-03 23:01:00 +0900

    feat : prettier 설정

---
+++ movie-react/src/Loading.jsx
@@ -0,0 +1,14 @@
+// react
+import React from 'react';
+// styled
+import styled from 'styled-components';
+
+const Loading = () => {};
+
+export default Loading;
+
+// style
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
