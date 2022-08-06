486a3993940fa962fe962b846577f447d1586d16
DevWonny <cjfdnjs1994@naver.com>
2022-08-06 22:17:58 +0900

    feat : naver movie api 연결 및 proxy 설정

---
+++ movie-react/src/api/NaverMovieAPI.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const NaverMovieAPI = async () => {
+  const headers = {
+    'X-Naver-Client-Id': '3K3Q44KwIDZ5eFYBFISX',
+    'X-Naver-Client-Secret': '1QALDriLde',
+  };
+  const res = await axios.get(`/v1/search/movie.json?query=아이언 맨`, { headers });
+};
+
+export default NaverMovieAPI;
